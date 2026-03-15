d3f1cd93c46fa125699be46dbf2244617dbca0e4
Unam Matheatau <matheatauunnam@gmail.com>
2026-03-15 23:27:27 +0200

    fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/thari-tech-website/", // This must match your GitHub repo name
+  base: "/", // This must match your GitHub repo name
 });
